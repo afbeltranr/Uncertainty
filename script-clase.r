5bdf0431a4522aef94526c665c0c53e07572f468
Andres Felipe <afbeltranr@unal.edu.co>
2021-05-31 20:19:26 -0500

    .

---
+++ script-clase.r
@@ -0,0 +1,16 @@
+library(chemometrics)
+data(glass)
+CaO <- glass[,'CaO']
+
+set.seed(1)
+n <- 1000 
+N <- 12
+promedios <- vector("numeric", n)
+for(i in 1:n){
+  
+ 	muestra[i] <- sample(caO, N)
+}
+
+
+
+
